0aebf711a8bf7390408116f98da75904bf9b6a51
Nikita Nikitenko <babun.babusha@gmail.com>
2013-09-09 14:04:20 +0300

    изменён:   bochsout.txt 	изменён:   bochsrc.txt 	изменён:   floppy.img 	изменён:   run_bochs.sh 	изменён:   update_image.sh 	BIOS-bochs-latest 	build

--- run_bochs.sh
+++ run_bochs.sh
@@ -2,6 +2,6 @@
 
 # run_bochs.sh
 # mounts the correct loopback device, runs bochs, then unmounts.
-sudo /sbin/losetup /dev/loop0 floppy.img
+sudo /sbin/losetup /dev/loop$1 floppy.img
 sudo bochs -f bochsrc.txt
-sudo /sbin/losetup -d /dev/loop0
+sudo /sbin/losetup -d /dev/loop$1
