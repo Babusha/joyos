26024b76bd3eb4bb6734e68f685dcd8ac78534c6
Nikita Nikitenko <babun.babusha@gmail.com>
2013-09-09 13:24:40 +0300

    изменён:   bochsrc.txt 	изменён:   floppy.img 	изменён:   src/Makefile 	изменён:   update_image.sh 	VGABIOS-elpin-2.40 	bochsout.txt

--- update_image.sh
+++ update_image.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo /sbin/losetup /dev/loop0 floppy.img
-sudo mount /dev/loop0 /mnt2
+sudo /sbin/losetup /dev/loop$1 floppy.img
+sudo mount /dev/loop$1 /mnt2
 sudo cp src/kernel /mnt2/kernel
 sudo cp initrd.img /mnt2/initrd
-sudo umount /dev/loop0
-sudo /sbin/losetup -d /dev/loop0
+sudo umount /dev/loop$1
+sudo /sbin/losetup -d /dev/loop$1
